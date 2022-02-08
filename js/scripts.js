059a120d98df15ad16d477b6b5ce27dc6000a40b
zackkb <98679473+zackkb@users.noreply.github.com>
2022-02-08 00:56:31 -0600

    Added array with attributes
    Created new code with an array of pokemon with various attributes including name, height, and type.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,38 @@
-alert('Hello world');
+// List of pokemon and their attributes
 
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [{
+    name: "Bulbasaur",
+    height: 0.7,
+    type: ["grass", "poison"]
+},
+
+{
+    name: "Ivysaur",
+    height: 1,
+    type: ["grass", "poison"]
+},
+
+{
+    name: "Venusaur",
+    height: 2,
+    type: ["grass", "poison"]
+},
+
+{
+    name: "Charmander",
+    height: 0.6,
+    type: ["fire"]
+},
+
+{
+    name: "Charmeleon",
+    height: 1.1,
+    type: ["fire"]
+},
+
+{
+    name: "Charizard",
+    height: 1.7,
+    type: ["fire", "flying"]
+},
+];
