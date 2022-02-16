b6fceaf07d7f052b96f0391b08d4ff1f7ac782be
zackkb <98679473+zackkb@users.noreply.github.com>
2022-02-15 21:57:15 -0600

    IIFE wrapping, changing for to forEach, syntax/grammar issues
    Wrapped pokemonList in IIFE;
    
    Cleaned up formatting and grammar;
    
    Added pokemonRepository;
    
    Changed for to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,69 @@
-// List of pokemon and their attributes
-let pokemonList = [{
-    name: "Bulbasaur",
-    height: 0.7,
-    type: ["grass", "poison"]
-},
+// IIFE for pokemonList
+let pokemonRepository = (function() {
+    // Array for pokemon
+    let pokemonList = [{
+            name: "Bulbasaur",
+            height: 0.7,
+            type: ["Grass", "Poison"]
+        },
 
-{
-    name: "Ivysaur",
-    height: 1,
-    type: ["grass", "poison"]
-},
+        {
+            name: "Ivysaur",
+            height: 1,
+            type: ["Grass", "Poison"]
+        },
 
-{
-    name: "Venusaur",
-    height: 2,
-    type: ["grass", "poison"]
-},
+        {
+            name: "Venusaur",
+            height: 2,
+            type: ["Grass", "Poison"]
+        },
 
-{
-    name: "Charmander",
-    height: 0.6,
-    type: ["fire"]
-},
+        {
+            name: "Charmander",
+            height: 0.6,
+            type: ["Fire"]
+        },
 
-{
-    name: "Charmeleon",
-    height: 1.1,
-    type: ["fire"]
-},
+        {
+            name: "Charmeleon",
+            height: 1.1,
+            type: ["Fire"]
+        },
 
-{
-    name: "Charizard",
-    height: 1.7,
-    type: ["fire", "flying"]
-},
-];
+        {
+            name: "Charizard",
+            height: 1.7,
+            type: ["Fire", "Flying"]
+        },
+    ];
 
-// Initialization "let i = 0", the condition "i < pokemonList.length;"
-
-// The action "i++ which is the equivalent of i = i + 1"
-
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write('<br>' + pokemonList[i].name + (' , height: ') + pokemonList[i].height);
-    //This handles heights greater >= to 1.0
-    if (pokemonList[i].height >= 1.0) {
-        document.write(' - That\'s a big Pokemon!');
+    //Prints details of pokemon
+    function add(pokemon) {
+        pokemonList.push(pokemon);
     }
-}
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll,
+
+    };
+})();
+
+
+pokemonRepository.add({
+    name: "Squirtle",
+    height: 0.5,
+    type: ["Water"],
+});
+
+pokemonRepository.getAll().forEach(function(pokemonList) {
+
+    if (pokemonList.height >= 1.5) {
+        document.write(' - That\'s a big Pokemon!');
+    } else document.write('<br>' + pokemonList.name + (' , height: ') + pokemonList.height);
+});
